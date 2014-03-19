4fc84507408284c00d563c874ca37c2874f80497
GrantNakamura <grantcnakamura@gmail.com>
2014-03-19 10:59:47 -0700

    Split jetcd into two parts

--- maven-collectors-build.sh
+++ maven-collectors-build.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 #checkout all modules
-mvn --non-recursive scm:checkout -Dmodule.name=jetcd
+mvn --non-recursive scm:bootstrap -Dmodule.name=jetcd-mods
 mvn --non-recursive scm:bootstrap -Dmodule.name=document-service-client-java
-
-# build jetcd with gradle
-cd jetcd
-./gradlew install
 
 # build collectors
 cd ..
 mvn clean install
 
-rm -rf jetcd
+rm -rf jetcd-mods
 rm -rf document-service-client-java
