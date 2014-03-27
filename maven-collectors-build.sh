c74eda75ee3d18ed2ad9902643fadccbd99e6f86
GrantNakamura <grantcnakamura@gmail.com>
2014-03-27 13:38:18 -0700

    Converting to use new etcd-java repo

--- maven-collectors-build.sh
+++ maven-collectors-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #checkout all modules
-mvn --non-recursive scm:bootstrap -Dmodule.name=jetcd-mods
+mvn --non-recursive scm:bootstrap -Dmodule.name=etcd-java
 mvn --non-recursive scm:bootstrap -Dmodule.name=document-service-client-java
 
 # build collectors
 cd ..
 mvn clean install
 
-rm -rf jetcd-mods
+rm -rf etcd-java
 rm -rf document-service-client-java
