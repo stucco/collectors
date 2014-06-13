44f445a47b5effc8cddfd03cb849dac1b7b1b983
Grant Nakamura <grant.nakamura@pnnl.gov>
2014-06-13 11:52:32 -0700

    Add simple scheduler

---
+++ src/main/java/gov/pnnl/stucco/utilities/FileChecksum.java
@@ -0,0 +1,91 @@
+package gov.pnnl.stucco.utilities;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+/** 
+ * Utility for generating a checksum (such as MD5 or SHA-1) for a file.
+ * 
+ * @author Grant Nakamura, June 2014
+ */
+public class FileChecksum {
+
+    /** Converts a byte array to a two-chars-per-byte hex string. */
+    private static String toHexString(byte[] bytes) {
+        StringBuilder builder = new StringBuilder(bytes.length * 2);
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        
+        return builder.toString();
+    }
+    
+    /**
+     * Computes the checksum of a file. This uses a buffer, so it avoids having
+     * to read the entire file into RAM. 
+     * 
+     * @param algorithm  The algorithm to use, such as MD5 or SHA-1.
+     * @param file       File to checksum.
+     * 
+     * @return Lowercase hexadecimal checksum string
+     */
+    public static String compute(String algorithm, File file) throws IOException, NoSuchAlgorithmException {
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            // Get the algorithm
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+        
+            // Read the file in chunks, updating checksum as we go
+            byte[] buffer = new byte[8192]; // 8K
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) > 0) {
+                digest.update(buffer, 0, bytesRead);
+            }
+            
+            // Get the result in hex format
+            byte[] checksumArray = digest.digest();            
+            String checksum = toHexString(checksumArray);
+            return checksum;
+        }
+    }
+    
+    public static void main(String[] args) {
+        // Parse command line
+        
+        if (args.length != 2) {
+            System.err.println("Usage: FileChecksum <hash> <file>");
+            System.exit(1);
+        }
+        
+        String algorithm = args[0];
+        File file = new File(args[1]);
+        
+        if (!file.exists()) {
+            System.err.format("\"%s\" not found", file);
+            System.exit(2);
+        }
+        
+        try {
+            // Get the checksum
+            String checksum = FileChecksum.compute(algorithm, file);
+            System.out.println(checksum);
+        } 
+        catch (NoSuchAlgorithmException nsae) {
+            System.err.format("Unrecognized algorithm: %s%n", algorithm);
+            System.exit(3);
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.exit(0);
+    }
+
+
+}
