4d3b4cd35823dfea2c937bdf444fd18586e4c777
Shawn Bohn <sjfawks@gmail.com>
2014-10-17 17:09:31 +0000

    updated the scheduler-vm to take a section argument

--- scheduler-vm.sh
+++ scheduler-vm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# In order to run this script you should specify the section you want for this to run
+# Choices are: demo-load, production
 # to be run within the VM
-java -Xmx2048m -jar /stucco/collectors/target/scheduler.jar -file /stucco/config/stucco.yml -section scheduler
+#java -Xmx2048m -jar /stucco/collectors/target/scheduler.jar -file /stucco/config/stucco.yml -section $1
+java -Xmx2048m -jar /stucco/collectors/target/scheduler.jar -section $1
