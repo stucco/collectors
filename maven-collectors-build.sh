bc5357ea3decb61f4b34fbd6cd71d42ffc299e28
John Goodall <jgoodall@gmail.com>
2014-11-11 20:53:52 -0500

    reduce maven log level to only show errors

--- maven-collectors-build.sh
+++ maven-collectors-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #checkout all modules
-mvn --non-recursive scm:bootstrap -Dmodule.name=etcd-java
-mvn --non-recursive scm:bootstrap -Dmodule.name=document-service-client-java
+mvn -q --non-recursive scm:bootstrap -Dmodule.name=etcd-java
+mvn -q --non-recursive scm:bootstrap -Dmodule.name=document-service-client-java
 
 # build collectors
-mvn clean install
+mvn -q clean install
 
 rm -rf etcd-java
 rm -rf document-service-client-java
