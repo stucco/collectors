7ba417be316a1ae6c6fcfaa0013449d6d796ad7e
John Goodall <jgoodall@gmail.com>
2014-11-14 15:13:38 -0500

    building jar package now

--- maven-collectors-build.sh
+++ maven-collectors-build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-#checkout all modules
+# checkout all modules
 mvn -q --non-recursive scm:bootstrap -Dmodule.name=etcd-java
 mvn -q --non-recursive scm:bootstrap -Dmodule.name=document-service-client-java
 
 # build collectors
-mvn -q clean install
+mvn -q clean install -Dmaven.test.skip=true
+mvn -q clean package
 
+# cleanup
 rm -rf etcd-java
 rm -rf document-service-client-java
