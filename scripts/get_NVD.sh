150f5d29f219b561aeb7503e14359a6b2e4b93bf
euf <euf@ornl.gov>
2016-09-07 15:08:08 -0400

    workaround for NVD SSL issue

---
+++ scripts/get_NVD.sh
@@ -0,0 +1,5 @@
+mkdir -p tmp
+cd tmp
+#wget https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-2013.xml.gz
+wget https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-2014.xml.gz
+#wget https://nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-Recent.xml.gz
